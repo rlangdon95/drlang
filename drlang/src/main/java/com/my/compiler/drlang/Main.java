fef7e1c9d3e67f6c33c0e86b86f95b00b946fddf
am250135 <am250135@teradata.com>
2019-11-27 14:31:28 +0530

    First commit part 2

--- drlang/src/main/java/com/my/compiler/drlang/Main.java
+++ drlang/src/main/java/com/my/compiler/drlang/Main.java
@@ -4,10 +4,10 @@ package com.my.compiler.drlang;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class Main {
+
+    public static void main( String[] args ) {
+        
         System.out.println( "Hello World!" );
     }
 }
